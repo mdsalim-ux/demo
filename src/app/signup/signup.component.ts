4173ac1b3100b8883c976ea4f4a7d555810c41fe
mdsalimportfolio <mdsalimalam8@gmail.com>
2023-06-22 21:55:45 +0530

    hh

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToasterService } from '../services/toaster/toaster.service';
 import { TransationModule } from '../services/transation/transation.module';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -12,8 +14,11 @@ export class SignupComponent implements OnInit {
   emailFormControl!:FormGroup
   msg: any;
 
-constructor(private formBuilder:FormBuilder,private _notification:ToasterService,private _translate:TransationModule){
-  
+constructor(private formBuilder:FormBuilder,
+  private _dialogRef:MatDialogRef<any>,
+  private _notification:ToasterService,
+  private _translate:TransationModule,
+  private http:HttpClient){
 }
 
 
@@ -21,24 +26,21 @@ constructor(private formBuilder:FormBuilder,private _notification:ToasterService
     this.emailFormControl=this.formBuilder.group({
       username:['',Validators.required],
       password:['',Validators.required]
-
     }) 
    }
 
    onClick(){
-
-    if(    this.emailFormControl.valid      ){
-      this.msg=(this._translate.getTranslatelang('Pas'))
-      this._notification.error(this._translate.getTranslatelang('Pas'),'')
-      this._notification.info(this._translate.getTranslatelang('Pas'),'')
-  
-      this._notification.success(this._translate.getTranslatelang('Pas'),'')
-  
-      this._notification.warning(this._translate.getTranslatelang('Pas'),'')
-  
-      console.log( this.emailFormControl.valid,'Check Status')
-    }
+    this.emailFormControl.markAllAsTouched()
+    if(this.emailFormControl.valid){
+      this._notification.success(this._translate.getTranslatelang('Sign-Up Successfully'),'')
+      this._dialogRef.close(true);
+       let url="https://localhost:1234/api/Student/GetStudents"
+       this.http.get(url).subscribe(res=>{
+        console.log(res,'res')
+       });
+      }
     else{
+      this._notification.error(this._translate.getTranslatelang('Please enter the User Name or Password'),'')
       return
     }
   }
